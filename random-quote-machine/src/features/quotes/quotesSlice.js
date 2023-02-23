d62a7e3daab48f1634cce94505bb6f95ee434df1
Federico Barberon <jfedericobarberonj@gmail.com>
2023-02-23 00:44:51 +0000

    Add random quote machine proyect

---
+++ random-quote-machine/src/features/quotes/quotesSlice.js
@@ -0,0 +1,43 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
+import { getRandomNumber } from '../../utils/getRandomNumber'
+
+const QUOTES_URL = `https://quotable.io/quotes?page=${getRandomNumber(103)}`
+
+export const fetchQuotes = createAsyncThunk('quotes/fetchQuotes', async () => {
+  const data = await fetch(QUOTES_URL)
+  const quotes = await data.json()
+
+  return quotes
+})
+
+const initialState = {
+  quotes: [],
+  status: 'idle',
+  error: null
+}
+
+const quotesSlice = createSlice({
+  name: 'quotes',
+  initialState,
+  extraReducers: (builder) => {
+    builder.addCase(fetchQuotes.pending, (state, action) => {
+      state.status = 'loading'
+    })
+
+    builder.addCase(fetchQuotes.fulfilled, (state, action) => {
+      state.status = 'succeeded'
+      state.quotes = action.payload.results
+    })
+
+    builder.addCase(fetchQuotes.rejected, (state, action) => {
+      state.status = 'error'
+      state.error = action.error.message
+    })
+  }
+})
+
+export const getQuotes = (state) => state.quotes.quotes
+export const getQuotesStatus = (state) => state.quotes.status
+export const getQuotesError = (state) => state.quotes.error
+
+export default quotesSlice.reducer
